915b51388862e3d47faf6fc73ed85e8a6f1c307a
MatiCBL <brzoza.mathew@gmail.com>
2023-12-28 22:23:54 +0000

    'added-logic-behind-creating-a-new-entry-and-creating-related-url-with-entry-id-by-using-useRouter-hook.-Also-created-API-responsible-for-handling-new-entry-creation-and-related-function-in-utils-so-the-api-can-be-called'

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { createNewEntry } from "@/utils/api";
+import { useRouter } from "next/navigation";
+
 const NewEntryCard = () => {
+  const router = useRouter();
+
+  const handleOnClick = async () => {
+    const data = await createNewEntry();
+    router.push(`/journal/${data.id}`);
+  };
+
   return (
     <div
       className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
