be7f5fb33903c4b3d56aa87e7017a30d4589fa11
MatiCBL <brzoza.mathew@gmail.com>
2023-12-30 21:48:04 +0000

    'logic-added-for-consistent-prompts-creation-with-openAI'

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,11 +1,12 @@
 import EntryCard from "@/components/EntryCard";
 import NewEntryCard from "@/components/NewEntryCard";
+import { analyze } from "@/utils/ai";
 import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 import Link from "next/link";
 
 const getEntries = async () => {
-  const user = getUserByClerkID();
+  const user = await getUserByClerkID();
   const entries = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
@@ -14,6 +15,12 @@ const getEntries = async () => {
       createdAt: "desc",
     },
   });
+
+  await analyze(`I'm going to give you an journal entry, I want you to analyze for a few things. I need the mood, a summary, what the subject is, and a color representing the mood. You need to respond back with formatted JSON like so: {"mood": "", "subject": "", "color": "", "negative": ""}.
+  
+  entry:
+  Today wa a really great day. I finally was able to grab that pair of shoes I have been dying to get. 
+  `);
 
   return entries;
 };
